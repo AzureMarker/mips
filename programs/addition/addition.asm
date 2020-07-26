ac9ea88a8be36827d06bc48b69d649b06cb44946
Mcat12 <newtoncat12@yahoo.com>
2020-07-26 16:29:43 -0400

    Update addition.asm to use li, and add addition.text
    addition.text contains just the text (code). addition.out uses rsim's
    module format.

--- programs/addition/addition.asm
+++ programs/addition/addition.asm
@@ -4,7 +4,7 @@
 .globl main
 
 main:
-	addi $t0, $zero, 1      # $t0 = 1
-	addi $t1, $zero, 10     # $t1 = 10
-	add $t2, $t0, $t1      # $t2 = $t0 + $t1
+	li $t0, 1		# $t0 = 1
+	li $t1, 10		# $t1 = 10
+	add $t2, $t0, $t1	# $t2 = $t0 + $t1
 
