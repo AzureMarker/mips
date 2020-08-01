9da29313ccb3b0ace09ca02eb3222a142032b586
Mcat12 <newtoncat12@yahoo.com>
2020-08-01 13:14:19 -0400

    Simplify add_unsigned a bit

--- mips-simulator/src/math.rs
+++ mips-simulator/src/math.rs
@@ -1,6 +1,6 @@
 pub fn add_unsigned(a: u32, b: i32) -> u32 {
     if b < 0 {
-        a.wrapping_sub((b * -1) as u32)
+        a.wrapping_sub(-b as u32)
     } else {
         a.wrapping_add(b as u32)
     }
