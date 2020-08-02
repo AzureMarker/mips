1d366aa56755fcac0b2b66cf89c0726f7463445c
Mcat12 <newtoncat12@yahoo.com>
2020-08-02 11:36:27 -0400

    Parse arguments with structopt

--- mips-simulator/src/main.rs
+++ mips-simulator/src/main.rs
@@ -1,17 +1,30 @@
+use mips_simulator::config::Config;
 use mips_simulator::rsim::RsimModule;
 use mips_simulator::Processor;
 use std::error::Error;
+use std::fs;
 use std::io::Cursor;
-use std::{env, fs};
-use mips_simulator::config::Config;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct CliArgs {
+    #[structopt(long)]
+    disable_delay_slots: bool,
+
+    #[structopt(parse(from_os_str))]
+    file_path: PathBuf,
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file_path = env::args().nth(1).expect("Must provide a file argument");
-    let file_data = fs::read(file_path)?;
+    let args = CliArgs::from_args();
+    let file_data = fs::read(&args.file_path)?;
     let module = RsimModule::parse(&mut Cursor::new(file_data))?;
     println!("Loaded module with header: {:?}", module.header);
 
-    let mut processor = Processor::new(Config::default());
+    let mut processor = Processor::new(Config {
+        disable_delay_slots: args.disable_delay_slots,
+    });
     processor.text_segment(module.text_section());
     processor.read_only_data_segment(module.read_only_data_section());
     processor.set_entry(module.header.entry);
