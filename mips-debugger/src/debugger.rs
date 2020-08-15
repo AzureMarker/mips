f3c7348a0b85233ea210e7ed1367be76c117cd4d
Mcat12 <newtoncat12@yahoo.com>
2020-08-15 11:15:29 -0400

    Add color to the print register output

--- mips-debugger/src/debugger.rs
+++ mips-debugger/src/debugger.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use mips_simulator::Processor;
 use std::io;
 use std::io::Write;
@@ -109,49 +110,50 @@ impl Debugger {
     }
 
     fn print_registers(&self) {
+        #[rustfmt::skip]
         println!(
             "Program Counter = 0x{:08x}\n\
-             0  = $zero = 0x{:08x} 8  = $t0 = 0x{:08x} 16 = $s0 = 0x{:08x} 24 = $t8 = 0x{:08x}\n\
-             1  = $at   = 0x{:08x} 9  = $t1 = 0x{:08x} 17 = $s1 = 0x{:08x} 25 = $t9 = 0x{:08x}\n\
-             2  = $v0   = 0x{:08x} 10 = $t2 = 0x{:08x} 18 = $s2 = 0x{:08x} 26 = $k0 = 0x{:08x}\n\
-             3  = $v1   = 0x{:08x} 11 = $t3 = 0x{:08x} 19 = $s3 = 0x{:08x} 27 = $k1 = 0x{:08x}\n\
-             4  = $a0   = 0x{:08x} 12 = $t4 = 0x{:08x} 20 = $s4 = 0x{:08x} 28 = $gp = 0x{:08x}\n\
-             5  = $a1   = 0x{:08x} 13 = $t5 = 0x{:08x} 21 = $s5 = 0x{:08x} 29 = $sp = 0x{:08x}\n\
-             6  = $a2   = 0x{:08x} 14 = $t6 = 0x{:08x} 22 = $s6 = 0x{:08x} 30 = $fp = 0x{:08x}\n\
-             7  = $a3   = 0x{:08x} 15 = $t7 = 0x{:08x} 23 = $s7 = 0x{:08x} 31 = $ra = 0x{:08x}",
+             {}  = {} = 0x{:08x} {}  = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}\n\
+             {}  = {}   = 0x{:08x} {}  = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}\n\
+             {}  = {}   = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}\n\
+             {}  = {}   = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}\n\
+             {}  = {}   = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}\n\
+             {}  = {}   = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}\n\
+             {}  = {}   = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}\n\
+             {}  = {}   = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x} {} = {} = 0x{:08x}",
             self.processor.program_counter,
-            self.processor.registers.get(0),
-            self.processor.registers.get(8),
-            self.processor.registers.get(16),
-            self.processor.registers.get(24),
-            self.processor.registers.get(1),
-            self.processor.registers.get(9),
-            self.processor.registers.get(17),
-            self.processor.registers.get(25),
-            self.processor.registers.get(2),
-            self.processor.registers.get(10),
-            self.processor.registers.get(18),
-            self.processor.registers.get(26),
-            self.processor.registers.get(3),
-            self.processor.registers.get(11),
-            self.processor.registers.get(19),
-            self.processor.registers.get(27),
-            self.processor.registers.get(4),
-            self.processor.registers.get(12),
-            self.processor.registers.get(20),
-            self.processor.registers.get(28),
-            self.processor.registers.get(5),
-            self.processor.registers.get(13),
-            self.processor.registers.get(21),
-            self.processor.registers.get(29),
-            self.processor.registers.get(6),
-            self.processor.registers.get(14),
-            self.processor.registers.get(22),
-            self.processor.registers.get(30),
-            self.processor.registers.get(7),
-            self.processor.registers.get(15),
-            self.processor.registers.get(23),
-            self.processor.registers.get(31),
+            "0".blue(), "$zero".yellow(), self.processor.registers.get(0),
+            "8".blue(), "$t0".yellow(), self.processor.registers.get(8),
+            "16".blue(), "$s0".yellow(), self.processor.registers.get(16),
+            "24".blue(), "$t8".yellow(), self.processor.registers.get(24),
+            "1".blue(), "$at".yellow(), self.processor.registers.get(1),
+            "9".blue(), "$t1".yellow(), self.processor.registers.get(9),
+            "17".blue(), "$s1".yellow(), self.processor.registers.get(17),
+            "25".blue(), "$t9".yellow(), self.processor.registers.get(25),
+            "2".blue(), "$v0".yellow(), self.processor.registers.get(2),
+            "10".blue(), "$t2".yellow(), self.processor.registers.get(10),
+            "18".blue(), "$s2".yellow(), self.processor.registers.get(18),
+            "26".blue(), "$k0".yellow(), self.processor.registers.get(26),
+            "3".blue(), "$v1".yellow(), self.processor.registers.get(3),
+            "11".blue(), "$t3".yellow(), self.processor.registers.get(11),
+            "19".blue(), "$s3".yellow(), self.processor.registers.get(19),
+            "27".blue(), "$k1".yellow(), self.processor.registers.get(27),
+            "4".blue(), "$a0".yellow(), self.processor.registers.get(4),
+            "12".blue(), "$t4".yellow(), self.processor.registers.get(12),
+            "20".blue(), "$s4".yellow(), self.processor.registers.get(20),
+            "28".blue(), "$gp".yellow(), self.processor.registers.get(28),
+            "5".blue(), "$a1".yellow(), self.processor.registers.get(5),
+            "13".blue(), "$t5".yellow(), self.processor.registers.get(13),
+            "21".blue(), "$s5".yellow(), self.processor.registers.get(21),
+            "29".blue(), "$sp".yellow(), self.processor.registers.get(29),
+            "6".blue(), "$a2".yellow(), self.processor.registers.get(6),
+            "14".blue(), "$t6".yellow(), self.processor.registers.get(14),
+            "22".blue(), "$s6".yellow(), self.processor.registers.get(22),
+            "30".blue(), "$fp".yellow(), self.processor.registers.get(30),
+            "7".blue(), "$a3".yellow(), self.processor.registers.get(7),
+            "15".blue(), "$t7".yellow(), self.processor.registers.get(15),
+            "23".blue(), "$s7".yellow(), self.processor.registers.get(23),
+            "31".blue(), "$ra".yellow(), self.processor.registers.get(31),
         );
     }
 }
