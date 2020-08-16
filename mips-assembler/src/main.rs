ea5d3f247041788832697571a22db8320b71d5e5
Mcat12 <newtoncat12@yahoo.com>
2020-08-16 19:24:15 -0400

    Pretty-print the test program

--- mips-assembler/src/main.rs
+++ mips-assembler/src/main.rs
@@ -7,8 +7,14 @@ mod ast;
 
 fn main() {
     println!(
-        "{:?}",
-        parser::ProgramParser::new()
-            .parse("# Test Comment\nMY_CONSTANT=1+1\n.data\n.text\n.globl main\nmain:")
+        "{:#?}",
+        parser::ProgramParser::new().parse(
+            "# Test Comment\n\
+            MY_CONSTANT=1+1\n\
+            .data\n\
+            .text\n\
+            .globl main\n\
+            main:\n"
+        )
     );
 }
