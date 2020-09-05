a0b16d33c99e9e5d25bba1d0e234144eab7ef1eb
Mcat12 <newtoncat12@yahoo.com>
2020-09-05 18:20:31 -0400

    Load assembly file from path argument

--- mips-assembler/src/main.rs
+++ mips-assembler/src/main.rs
@@ -1,25 +1,38 @@
 #[macro_use]
 extern crate lalrpop_util;
 
+use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
 lalrpop_mod!(parser);
 
 mod ast;
 
-fn main() {
-    println!(
-        "{:#?}",
-        parser::ProgramParser::new().parse(
-            "# Test Comment\n\
-            MY_CONSTANT=1+1\n\
-            .data\n\
-            .space MY_CONSTANT*4\n\
-            .word 2\n\
-            .text\n\
-            .globl main\n\
-            main:\n\
-                li $t0, 1		# $t0 = 1\n\
-                li $t1, 10		# $t1 = 10\n\
-                add $t2, $t0, $t1	# $t2 = $t0 + $t1\n"
-        )
-    );
+#[derive(StructOpt)]
+struct CliArgs {
+    #[structopt(parse(from_os_str))]
+    input_file: PathBuf,
+
+    #[structopt(
+        parse(from_os_str),
+        long = "output",
+        short = "o",
+        default_value = "out.obj"
+    )]
+    output_file: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Setup logging and parse CLI args
+    env_logger::init();
+    let args = CliArgs::from_args();
+
+    // Load the assembly file
+    let file_str = fs::read_to_string(&args.input_file)?;
+
+    println!("{:#?}", parser::ProgramParser::new().parse(&file_str));
+
+    Ok(())
 }
